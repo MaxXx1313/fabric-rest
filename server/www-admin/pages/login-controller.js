303de8ac96063cfca42bbf373b6afa9c707dad5c
MaxXx1313 <maksim.chertkov@gmail.com>
2018-07-11 07:41:16 -0500

    indicator for login screen

--- server/www-admin/pages/login-controller.js
+++ server/www-admin/pages/login-controller.js
@@ -6,16 +6,24 @@ function LoginController($scope, UserService, $state, ApiService) {
   var ctl = this;
 
   ctl.config = null;
+  ctl.inProgress = false;
 
   // ApiService.getConfig().then(function(config){
   //   ctl.config = config;
   // });
 
-  ctl.signUp = function(user){
+  ctl.signUp = function(user) {
+    ctl.inProgress = true;
     return UserService.signUp(user)
-    .then(function(){
-      $state.go('app.query');
-    });
+      .then(function(){
+        $state.go('app.query').then(()=>{
+          ctl.inProgress = false;
+        });
+      })
+      .catch(function(e){
+        ctl.inProgress = false;
+        // throw e;
+      })
   };
 
 }
