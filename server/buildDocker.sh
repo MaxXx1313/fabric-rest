bdcb2bc321d7ce10c69d8418f53390f739c7db44
MaxXx1313 <maksim.chertkov@gmail.com>
2018-07-17 10:44:23 -0500

    build docker script

--- server/buildDocker.sh
+++ server/buildDocker.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-VERSION=$(jq -r .version package.json)
-TAG="maxxx1313/fabric-rest:$VERSION"
+
+which jq 2>/dev/null
+retcode=$?
+#echo $retcode
+
+set -e
+if [ $retcode -eq 0 ]; then
+  VERSION=$(jq -r .version package.json);
+else
+  echo "WARN: no 'jq' found. Fallback to unstable variant"
+  echo "Please, install 'jq' because it's more reliable for getting json data"
+  VERSION=$(cat package.json |grep '"version"'|cut -d':' -f2- | grep -oP '".*"'|sed s/\"//g);
+fi;
+
+TAG="maxxx1313/fabric-rest:$VERSION-ibp"
 echo "Building $TAG"
-docker build -t $TAG --label com.altoros.version="$VERSION" .
+
+docker build -t "$TAG" --label version="$VERSION" .
+
+docker tag "$TAG" "ibp"
+
+echo "Done!"
+echo
+echo "Push images:"
+echo "  docker push $TAG"
+echo "  docker push ibp"
+
 
